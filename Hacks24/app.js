f409f332d5f3cf2d472b76042aa8fd6b6a82acbb
Sean Pandaleon <112599537+Sean-Panda@users.noreply.github.com>
2024-10-19 16:23:06 -0400

    AWS first steps

---
+++ Hacks24/app.js
@@ -0,0 +1,4 @@
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports'; // adjust the path as necessary
+
+Amplify.configure(awsconfig);
---
+++ Hacks24/aws-exports.js
@@ -0,0 +1,20 @@
+const awsconfig = {
+    Auth: {
+      identityPoolId: 'us-east-1:614b95d4-9ab2-4180-b1b2-3b4bdeeb6fcc', // Your Identity Pool ID
+      region: 'us-east-1', // Your AWS region
+      userPoolId: 'us-east-1_nMk0ehv5k', // Your User Pool ID
+      userPoolWebClientId: '1ugudas0pvlj8qj6lrd7ndoh29', // Your App Client ID
+    },
+    API: {
+        endpoints: [
+          {
+            name: "DynamoDBAPI",
+            endpoint: "https://YOUR_API_ENDPOINT", // replace with actual endpoint
+            region: "YOUR_AWS_REGION"
+          }
+        ]
+      }
+  };
+  
+  export default awsconfig;
+  
